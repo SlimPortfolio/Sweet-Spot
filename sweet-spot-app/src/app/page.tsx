2a2216d7f4d346c400e02a68f9f0af708175db3a
Steven Lim <stevenylim17@gmail.com>
2024-12-07 15:40:07 -0600

    home page completed with carousel and get started button, as well as routing.

--- sweet-spot-app/src/app/page.tsx
+++ sweet-spot-app/src/app/page.tsx
@@ -2,17 +2,12 @@
 
 import Image from "next/image";
 import logo from "../images/SweetSpot WordMark@3x.png";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
-import { Card, CardContent } from "@/components/ui/card";
 import { CarouselPlugin } from "@/components/carousel-plugin";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <div className="font-inriaSans flex flex-col items-center">
       <Image
@@ -21,11 +16,18 @@ export default function Home() {
         className="mx-2 h-auto"
         style={{ height: "auto", width: "500px" }}
       ></Image>
-      <h1 className="text-xl w-[48rem] text-center my-5">
+      <h1 className="text-2xl w-[48rem] text-center my-5">
         SweetSpot is a tool for Christian Worship Leaders to find a suitable key
         for their vocalists to sing songs in.
       </h1>
       <CarouselPlugin />
+      <Button
+        variant="destructive"
+        className="bg-[#e54f46] font-bold mt-6 font-sans drop-shadow-md"
+        onClick={() => router.push("/key-finder")}
+      >
+        GET STARTED
+      </Button>
     </div>
   );
 }
